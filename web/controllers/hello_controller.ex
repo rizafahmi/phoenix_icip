0a02475abbdb7a98148e7efb06d77919bda29217
Riza Fahmi <rizafahmi@gmail.com>
2015-06-04 16:06:28 +0700

    Finish controller part

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,18 +2,23 @@ defmodule PhoenixIcip.HelloController do
   use PhoenixIcip.Web, :controller
 
   plug :action
-  plug :render
 
   def index conn, _params do
-    conn
-    |> assign(:message, "Welcome back!")
-    |> assign(:name, "Riza")
-    # |> put_flash(:info, "Welcome to Phoenix, from flash")
-    # |> put_flash(:error, "Let's pretend we have error")
-    # |> render "index.html"
+    redirect conn, to: redirect_test_path(conn, :redirect_test)
+    # redirect conn, external: "http://elixir-lang.org/"
+  end
+
+  def redirect_test conn, _params do
+    text conn, "Redirect!"
   end
 
   def show conn, %{"messenger" => messenger} do
     render conn, "show.html", messenger: messenger
+  end
+
+  def listing conn, _params do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(201, "")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,7 @@ defmodule PhoenixIcip.Router do
   use PhoenixIcip.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "text"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
@@ -17,6 +17,8 @@ defmodule PhoenixIcip.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
+    get "/redirect_test", HelloController, :redirect_test, as: :redirect_test
+    get "/list", HelloController, :listing
     get "/hello/:messenger", HelloController, :show
     resources "users", UserController do
       resources "posts", PostController
